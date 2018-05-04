0ba6a701ba19b3c1e60334916cba2448da51ad13
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-05-04 17:36:59 -0500

    Refactor - Separadas las rutas en un fichero central

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
 const express = require('express');
+const routes = require('./routes');
+
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', function (request, response) {
-    response.send(`Hello on port ${PORT}`);
-});
+app.use('/', routes);
 
 app.listen(PORT, function() {
     console.log(`Express server started on ${PORT}`);
