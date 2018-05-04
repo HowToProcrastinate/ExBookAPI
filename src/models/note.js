239d6e9e74435c0192a0cf1b3eeecd00bb4116d5
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-05-04 18:58:19 -0500

    Modelo 'Notes' - Agregado el modelo Notes - Agregados los metodos GET y POST provisionales

---
+++ src/models/note.js
@@ -0,0 +1,31 @@
+let notes = [];
+notes.push({
+    'title': 'Note 1',
+    'body': 'Content'
+});
+notes.push({
+    'title': 'Note 2',
+    'body': 'Content'
+});
+notes.push({
+    'title': 'Note 3',
+    'body': 'Content'
+});
+notes.push({
+    'title': 'Note 4',
+    'body': 'Content'
+});
+
+module.exports = /** @class */ class Note{
+    
+    /**
+     * @returns {Array<String>} Listado de Notas
+     */
+    static list(){
+        return notes;
+    }
+
+    static get(id){
+        return (id > notes.length) ? -1 : notes[id];
+    }
+};
