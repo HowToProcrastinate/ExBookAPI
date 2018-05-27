772cdcffeac984f77a5e7cde0fe80a4f4aec7b94
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-05-26 22:00:47 -0500

    Fix: Cors

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use('/', routes);
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/', routes);
 
 app.listen(PORT, function() {
     if (process.env.NODE_ENV !== 'test') {
