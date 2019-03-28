173df93157b6008c04df1c11d5ef364bc9c78dd2
Esteban Dario Barboza Primera <dario0117@gmail.com>
2019-03-27 21:20:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const routes = require('./routes');
 const setAuth = require('./auth');
+const cors = require('cors');
 
 setAuth();
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routes);
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 mongoConnect();
 
